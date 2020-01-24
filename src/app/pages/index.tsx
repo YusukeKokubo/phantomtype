07a000c5535f3af2b9b1b07bc22b04c51772c984
YusukeKokubo <yusuke.kokubo@gmail.com>
2020-01-24 21:01:30 +0900

    update link to cover image

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -10,11 +10,11 @@ const view = () =>
   <App>
     <section className={css.splash}>
       <img className={css.splash_image}
-           src="https://storage.googleapis.com/phantomtype-180814.appspot.com/splash/splash-1.jpg"/>
+        src="https://firebasestorage.googleapis.com/v0/b/phantomtype.appspot.com/o/kyoto%2FDSCF8041.jpg?alt=media" />
       <div className={css.title}>
         <img src='/logomark-white.svg' className={css.logo} />
-        <h1 className={ css.siteTitle }>PHANTOM TYPE</h1>
-        <p className={ css.description }>a Japan photo gallery.</p>
+        <h1 className={css.siteTitle}>PHANTOM TYPE</h1>
+        <p className={css.description}>a Japan photo gallery.</p>
       </div>
     </section>
     <Nav />
