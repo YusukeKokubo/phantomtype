7b1febf3a810d135b8d91cb999e6e0d44e15d1c3
YusukeKokubo <yusuke.kokubo@gmail.com>
2020-02-15 16:26:45 +0900

    show like icon (WIP)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,11 +7,13 @@ const App = ({ children }: { children?: any }) => (
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' key='viewport' />
       <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css' />
+      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
       <link rel='stylesheet' href='https://fonts.googleapis.com/earlyaccess/notosansjp.css' />
+      <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
     </Head>
     <style jsx={true} global={true}>{`
       body {
-        font-family: 'Noto Sans JP', Helvetica, sans-serif;
+        font-family: 'Noto Sans JP';
         font-size: 100%;
         font-weight: 200;
         background: #000;
--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -1,3 +1,6 @@
+import { red } from '@material-ui/core/colors';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import css from './photo.module.css'
@@ -19,13 +22,21 @@ function PhotoView({ photo, align }: { photo: Photo, align: number }) {
         <source type='image/webp' srcSet={e.urls.webp} />
         <LazyLoadImage src={e.urls.lowQuality} className={css.Photo_image} />
       </picture>
-      <div className={`${css.exif} ${align === 1 ? css.exif_right : null}`}>
-        <span className={css.datetime}>{datetime(e.exif.DateTimeOriginal)}</span>
-        <span>{e.image.Make} {e.image.Model}</span>
-        <span>{e.exif.FocalLength} ({e.exif.FocalLengthIn35mmFormat})mm
-        / F{e.exif.FNumber} / {e.exif.ExposureTime}S</span>
-        <span>ISO {e.exif.ISO}</span>
-        <span>{e.exif.LensMake} {e.exif.LensModel}</span>
+      <div className={`${css.information} ${align === 1 ? css.exif_right : null}`}>
+        <div className={`${css.exif} ${align === 1 ? css.exif_right : null}`}>
+          <span className={css.datetime}>{datetime(e.exif.DateTimeOriginal)}</span>
+          <span>{e.image.Make} {e.image.Model}</span>
+          <span>{e.exif.FocalLength} ({e.exif.FocalLengthIn35mmFormat})mm
+          / F{e.exif.FNumber} / {e.exif.ExposureTime}S</span>
+          <span>ISO {e.exif.ISO}</span>
+          <span>{e.exif.LensMake} {e.exif.LensModel}</span>
+        </div>
+        <div className={css.social}>
+          <span>
+            <FavoriteBorder className={css.icon} style={{ fontSize: 25, color: red[900] }} />
+            <span className={css.like}>100</span>
+          </span>
+        </div>
       </div>
     </section>
   )
