9385967553f89fd44185ae917b5799b2e5486e86
YusukeKokubo <yusuke.kokubo@gmail.com>
2020-02-15 20:32:23 +0900

    Like controle by cookie

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -1,5 +1,7 @@
 import { red } from '@material-ui/core/colors';
+import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Cookies from 'js-cookie';
 
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
@@ -18,6 +20,9 @@ function onLike(fb: firebase.app.App, photo: Photo) {
   fb.firestore().collection('pics').doc(id).set({
     ...photo,
     like: (photo.like || 0) + 1
+  }).then(() => {
+    Cookies.set(id, "1")
+    console.log('Liked: ', id)
   })
   return null
 }
@@ -25,6 +30,8 @@ function onLike(fb: firebase.app.App, photo: Photo) {
 function PhotoView({ fb, photo, align }: { fb: firebase.app.App, photo: Photo, align: number }) {
   const e = photo
   console.log(e)
+  const id = `${photo.city}-${photo.filename}`
+  const liked = Cookies.get(id) == "1"
 
   return (
     <section className={`${css.Photo} ${align === 1 ? css.Photo_right : null}`}>
@@ -43,12 +50,18 @@ function PhotoView({ fb, photo, align }: { fb: firebase.app.App, photo: Photo, a
         </div>
         <div className={css.social}>
           <span>
-            <FavoriteBorder
-              onClick={() => onLike(fb, photo)}
-              className={css.icon}
-              style={{ fontSize: 25, color: red[900] }}
-            />
-            <span className={css.like}>{photo.like}</span>
+            {liked ?
+              <>
+                <Favorite className={css.icon} style={{ fontSize: 25, color: red[900] }} />
+                <span className={css.like}>{photo.like} likes</span>
+              </>
+              :
+              <FavoriteBorder
+                onClick={() => onLike(fb, photo)}
+                className={css.icon}
+                style={{ fontSize: 25, color: red[900] }}
+              />
+            }
           </span>
         </div>
       </div>
