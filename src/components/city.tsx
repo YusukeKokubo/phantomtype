2eed3012a7c8075dfd24b246f70c9cd9ea2e0d7e
YusukeKokubo <yusuke.kokubo@gmail.com>
2020-02-22 15:24:27 +0900

    randomize with seed

--- src/components/city.tsx
+++ src/components/city.tsx
@@ -7,11 +7,21 @@ import 'firebase/firestore'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import firebase from '../firebase'
 
+import './random'
+
+declare global {
+  // tslint:disable-next-line: interface-name
+  interface Math {
+    seed(seed: number): void;
+  }
+}
+
 const shuffle = ([...array]) => {
-  // for (let i = array.length - 1; i >= 0; i--) {
-  //   const j = Math.floor(Math.random() * (i + 1));
-  //   [array[i], array[j]] = [array[j], array[i]];
-  // }
+  Math.seed(new Date().getHours())
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
   return array;
 }
 
