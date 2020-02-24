d63c7cca17d4035d21edef5994e45bdf392f18cd
YusukeKokubo <yusuke.kokubo@gmail.com>
2020-02-24 20:15:20 +0900

    fixed Nextjs issue with MaterialUI
    see https://medium.com/@manakuro/ssr-with-next-js-styled-components-and-material-ui-b1e88ac11dfa

---
+++ src/pages/_document.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import NextDocument from 'next/document'
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
+
+// Magick from https://medium.com/@manakuro/ssr-with-next-js-styled-components-and-material-ui-b1e88ac11dfa
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const styledComponentSheet = new StyledComponentSheets()
+    const materialUiSheets = new MaterialUiServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            styledComponentSheet.collectStyles(
+              materialUiSheets.collect(<App {...props} />),
+            ),
+        })
+
+      const initialProps = await NextDocument.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key="styles">
+            {initialProps.styles}
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      }
+    } finally {
+      styledComponentSheet.seal()
+    }
+  }
+}
