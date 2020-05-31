b3ae4496a16b6a7ff7aeaf4b84c15380fc7bafed
YusukeKokubo <yusuke.kokubo@gmail.com>
2020-05-31 15:27:25 +0900

    use tailwind and fix like saving

--- src/components/like.tsx
+++ src/components/like.tsx
@@ -1,46 +1,13 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
-import { red } from '@material-ui/core/colors';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 import Cookies from 'js-cookie';
 import { Photo } from '../../@types/Photo';
-
-export const IconStyle = makeStyles(({ palette }: Theme) => createStyles({
-  YetLiked: {
-    margin: '-3px 2px',
-    fontSize: '2.0vw',
-    color: palette.text.primary,
-    '&:hover': {
-      cursor: 'pointer',
-      color: red[600],
-    },
-    '@media (max-width: 600px)': {
-      'font-size': '1.0rem',
-    },
-  },
-  Liked: {
-    margin: '-3px 2px',
-    fontSize: '2.0vw',
-    color: red[600],
-    '&:hover': {
-      cursor: 'pointer',
-      color: red[600],
-    },
-    '@media (max-width: 600px)': {
-      'font-size': '1.0rem',
-    },
-  },
-  LikeText: {
-    fontSize: '1.0rem',
-  },
-}))
+import { useState } from 'react';
 
 function onLike(fb: firebase.app.App, photo: Photo) {
   const id = `${photo.city}-${photo.filename}`
-  fb.firestore().collection('pics').doc(id).set({
-    ...photo,
+  const data = {
     like: (photo.like || 0) + 1,
-  }).then(() => {
+  }
+  fb.firestore().collection('pics').doc(id).set(data, { merge: true }).then(() => {
     Cookies.set(id, '1')
     console.log('Liked: ', id)
   })
@@ -49,10 +16,10 @@ function onLike(fb: firebase.app.App, photo: Photo) {
 
 function onUnLike(fb: firebase.app.App, photo: Photo) {
   const id = `${photo.city}-${photo.filename}`
-  fb.firestore().collection('pics').doc(id).set({
-    ...photo,
+  const data = {
     like: (photo.like || 0) - 1,
-  }).then(() => {
+  }
+  fb.firestore().collection('pics').doc(id).set(data, { merge: true }).then(() => {
     Cookies.set(id, '0')
     console.log('UnLiked: ', id)
   })
@@ -61,26 +28,25 @@ function onUnLike(fb: firebase.app.App, photo: Photo) {
 
 function LikeView({ fb, photo }: { fb: firebase.app.App, photo: Photo }) {
   const id = `${photo.city}-${photo.filename}`
-  const liked = Cookies.get(id) === '1'
-  const is = IconStyle()
+  const [liked, setLiked] = useState(Cookies.get(id) === '1')
+  const [likedCount, setLikedCount] = useState(photo.like)
 
   return (
-    <>
-      <span>
-        {liked ?
-          <Favorite
-            onClick={() => onUnLike(fb, photo)}
-            className={is.Liked}
-          />
-          :
-          <FavoriteBorder
-            onClick={() => onLike(fb, photo)}
-            className={is.YetLiked}
-          />
-        }
-      </span>
-      <span className={is.LikeText}>{photo.like || 0} like(s)</span>
-    </>
+    <div className={`flex flex-col liked-${liked}`}>
+      {liked ?
+        <i
+          onClick={() => { onUnLike(fb, photo); setLikedCount(likedCount - 1); setLiked(false) }}
+          className='material-icons mx-1 pt-2 text-2xl cursor-pointer hover:text-white sm:text-red-600'
+        >favorite</i>
+        :
+        <i onClick={() => { onLike(fb, photo); setLikedCount(likedCount + 1); setLiked(true) }}
+          className='material-icons mx-1 pt-2 text-2xl cursor-pointer hover:text-red-600'
+        >
+          favorite_border
+          </i>
+      }
+      <span className='text-base'>{likedCount || 0} like(s)</span>
+    </div>
   )
 }
 
