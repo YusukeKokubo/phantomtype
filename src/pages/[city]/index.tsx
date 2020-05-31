d50023652c5f7c6395860e3e1565be9ab799b504
YusukeKokubo <yusuke.kokubo@gmail.com>
2020-05-31 09:23:58 +0900

    SSG by Next.js 9.4!

--- src/pages/[city]/index.tsx
+++ src/pages/[city]/index.tsx
@@ -1,19 +1,46 @@
 import { NextPage } from 'next'
 
-import City from '../../components/city';
-import { FixedNav } from '../../components/Nav'
+import 'firebase/firestore'
+import firebase from 'firebase'
 
-const CityPage: NextPage<{ city: string }> = ({ city }) => {
+import { FixedNav } from '../../components/Nav'
+import { Photo } from '../../../@types/Photo';
+import PhotoView from '../../components/photo';
+
+import { shuffle } from '../../lib/shuffle'
+
+const CityPage: NextPage<{ city: string, picsData: any }> = ({ city, picsData }) => {
+  const pics = JSON.parse(picsData) as Photo[]
+  console.log(pics.map(p => p.filename))
   return (
     <>
       <FixedNav city={city} />
-      <City city={city} />
+      <div className='grid gap-16 grid-rows-1'>
+        <h2 className='mt-16 text-4xl text-center uppercase'>{city}</h2>
+        {
+          shuffle(pics).map((p, i) => {
+            return <PhotoView fb={firebase.app()} key={i} photo={p} align={i % 2} />;
+          })
+        }
+      </div>
     </>
   )
 };
-CityPage.getInitialProps = async (context) => {
-  const { city }: any = context.query
-  console.log(city)
-  return { city }
+
+export async function getStaticPaths() {
+  return {
+    paths: ['/kyoto', '/nagoya', '/kanazawa', '/matsushima'],
+    fallback: false
+  }
 }
+
+export async function getStaticProps({ params }) {
+  const db = await firebase.firestore().collection('pics').where('city', '==', params.city).get()
+  const pics = db.docs.map(d => d.data())
+
+  return {
+    props: { city: params.city, picsData: JSON.stringify(pics) }
+  }
+}
+
 export default CityPage;
