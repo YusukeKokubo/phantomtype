645c5946488078d7269dea2614d06429128bee7e
YusukeKokubo <yusuke.kokubo@gmail.com>
2021-06-17 18:42:20 +0900

    ncu

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   images: {
     deviceSizes: [320, 420, 768, 1024, 1200, 1400, 1680, 1800, 2400]
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  },
   reactStrictMode: true
 }
